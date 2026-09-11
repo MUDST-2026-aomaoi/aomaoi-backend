9e4a079f5801236984a1e3ec231e1f09ce9ecab3
Jiranan <>
2026-09-11 15:05:09 +0700

    add unit tests

--- src/test/java/com/aomaoi/backend/BackendApplicationTests.java
+++ src/test/java/com/aomaoi/backend/BackendApplicationTests.java
@@ -1,13 +1,14 @@
 package com.aomaoi.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        BackendApplication app = new BackendApplication();
+        assertNotNull(app);
+    }
 
 }

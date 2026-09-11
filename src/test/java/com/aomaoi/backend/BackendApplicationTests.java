8c4c30ad500ff884eef02abd059b8e70e0a0945c
Jiranan <>
2026-09-11 16:21:51 +0700

    hi

--- src/test/java/com/aomaoi/backend/BackendApplicationTests.java
+++ src/test/java/com/aomaoi/backend/BackendApplicationTests.java
@@ -2,13 +2,13 @@ package com.aomaoi.backend;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+//hi
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
         BackendApplication app = new BackendApplication();
-        assertNotNull(app);
+        assertNotNull(app); 
     }
 
 }
